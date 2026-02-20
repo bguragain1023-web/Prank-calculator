f350acf253beabf2f5492ee153bc2f72d0767b15
Brazesh Guragain <b.guragain1023@gmail.com>
2026-02-20 21:51:55 +1100

    Initial commit

---
+++ READ.md
@@ -0,0 +1,17 @@
+# Building prank Calculator Workflow :
+
+1. write HTML
+2. Apply CSS
+3. capture each button click
+4. get the value of the button and store it in a global variable
+5. show the string from the global variable
+6. make AC button work
+7. make = button work
+8. make the C button work
+9. Ignore the last operator and show the result
+10. prevent multiple operators to press
+11. allow only one "." per number set
+12. make it a prank by adding a random value
+13. add prank animation
+14. add button click effect
+15. Bind the keypress on the calculator
---
+++ script.js
@@ -0,0 +1,89 @@
+const allButtonsElm = document.querySelectorAll(".btn");
+let strDisplay = "";
+const displayElm = document.querySelector(".display");
+
+const operators = ["*", "+", "-", "/", "%"];
+let lastOperator = "";
+const audio = new Audio("./assets/pranked.mp3");
+
+const buttonAction = (value) => {
+  displayElm.classList.remove("prank");
+  if (value === "AC") {
+    strDisplay = "";
+    return display(strDisplay);
+  }
+
+  if (value === "=") {
+    //get the last character of the string
+    lastOperator = "";
+    const lastchar = strDisplay[strDisplay.length - 1];
+    //check if it is one of tjhe operators
+    if (operators.includes(lastchar)) {
+      strDisplay = strDisplay.slice(0, -1);
+    }
+
+    return displayTotal();
+  }
+
+  if (value === "C") {
+    strDisplay = strDisplay.slice(0, -1);
+    return display(strDisplay);
+  }
+
+  if (operators.includes(value)) {
+    lastOperator = value;
+    const lastchar = strDisplay[strDisplay.length - 1];
+    if (operators.includes(lastchar)) {
+      strDisplay = strDisplay.slice(0, -1);
+    }
+  }
+
+  if (value === ".") {
+    const lastIndexOperator = strDisplay.lastIndexOf(lastOperator);
+    const numberSet = strDisplay.slice(lastIndexOperator);
+    if (numberSet.includes(".")) {
+      return;
+    }
+
+    if (!lastOperator && strDisplay.includes(".")) {
+      return;
+    }
+  }
+
+  strDisplay += value;
+
+  display(strDisplay);
+};
+
+allButtonsElm.forEach((btn) => {
+  btn.addEventListener("mousedown", () => {
+    btn.style.scale = ".9";
+  });
+  btn.addEventListener("click", () => {
+    btn.style.scale = "1";
+    const value = btn.innerText;
+
+    buttonAction(value);
+  });
+});
+
+const display = (str) => {
+  displayElm.innerText = str || "0.0";
+};
+// calculate total
+const displayTotal = () => {
+  const extraValue = randValue();
+  if (extraValue) {
+    displayElm.classList.add("prank");
+    audio.play();
+  }
+
+  const total = eval(strDisplay) + extraValue;
+  strDisplay = total.toString();
+  display(strDisplay);
+};
+
+const randValue = () => {
+  const num = Math.round(Math.random() * 10);
+  return num < 3 ? num : 0;
+};
